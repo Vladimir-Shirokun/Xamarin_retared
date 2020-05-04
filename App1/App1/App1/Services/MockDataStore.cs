2fd9a4165ec11d99f30fd570855033da75cf0db1
packman <36705530+Vladimir-Shirokun@users.noreply.github.com>
2020-05-05 02:51:30 +0300

    Application ver 1.0
    just the most needed buttons and links

---
+++ App1/App1/App1/Services/MockDataStore.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using App1.Models;
+
+namespace App1.Services
+{
+    public class MockDataStore : IDataStore<Item>
+    {
+        readonly List<Item> items;
+
+        public MockDataStore()
+        {
+            items = new List<Item>()
+            {
+                new Item { Id = Guid.NewGuid().ToString(), Text = "Позвонить", Description="Позвонить контакту из записной книжки" },
+                new Item { Id = Guid.NewGuid().ToString(), Text = "Написать", Description="Отправить сообщение в WhatsApp" },
+                new Item { Id = Guid.NewGuid().ToString(), Text = "Интернет", Description="Поиск информации, социальные сети" }
+            };
+        }
+
+        public async Task<bool> AddItemAsync(Item item)
+        {
+            items.Add(item);
+
+            return await Task.FromResult(true);
+        }
+
+        public async Task<bool> UpdateItemAsync(Item item)
+        {
+            var oldItem = items.Where((Item arg) => arg.Id == item.Id).FirstOrDefault();
+            items.Remove(oldItem);
+            items.Add(item);
+
+            return await Task.FromResult(true);
+        }
+
+        public async Task<bool> DeleteItemAsync(string id)
+        {
+            var oldItem = items.Where((Item arg) => arg.Id == id).FirstOrDefault();
+            items.Remove(oldItem);
+
+            return await Task.FromResult(true);
+        }
+
+        public async Task<Item> GetItemAsync(string id)
+        {
+            return await Task.FromResult(items.FirstOrDefault(s => s.Id == id));
+        }
+
+        public async Task<IEnumerable<Item>> GetItemsAsync(bool forceRefresh = false)
+        {
+            return await Task.FromResult(items);
+        }
+    }
+}
---
+++ App1/App1/App1/ViewModels/AboutViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace App1.ViewModels
+{
+    public class AboutViewModel : BaseViewModel
+    {
+        public AboutViewModel()
+        {
+            Title = "Обучение";
+            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://docs.google.com/presentation/d/1UKbva7rfb7gTQXR8FsEEkWRk1EOSvZeuCxxEeXI6GMg/edit?usp=sharing"));
+        }
+
+        public ICommand OpenWebCommand { get; }
+    }
+}
